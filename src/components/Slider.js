c7d8d6955dacadb53c1d6ec9ef09f8612dfa99a4
irfan dayan <engr.irfandayan@gmail.com>
2022-03-25 18:24:04 +0500

    Add slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,10 +5,24 @@ import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftOutlinedIcon />
       </Arrow>
-      <Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="/images/slide-img-01.jpeg" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title> SUMMER SAL</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
         <ArrowRightOutlinedIcon />
       </Arrow>
     </Container>
@@ -19,6 +33,7 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  position: relative;
 `;
 
 const Arrow = styled.div`
@@ -26,6 +41,46 @@ const Arrow = styled.div`
   height: 50px;
   background-color: #fff7f7;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  opacity: 0.5;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
 `;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1``;
+const Desc = styled.p``;
+const Button = styled.button``;
 
 export default Slider;
